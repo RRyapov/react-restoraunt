9ddd6695a652871d96e044fe28adfecc866325ed
RRyapov <rryapov@gmail.com>
2023-09-10 12:15:47 +0300

    Добавлен логотип к элементам

--- src/components/body/square_tables/Squares.jsx
+++ src/components/body/square_tables/Squares.jsx
@@ -1,58 +1,47 @@
 import React from "react";
 import "./squares_style.css";
+import { SquaresLogotype } from "./SquaresLogotype";
 
 export const Squares = () => {
   return (
-    <div class="frame container">
-      <div class="frame__content">
-        <div class="frame__content-item">
-          <img
-            class="frame__content-item_pic"
-            src="frame_pic.png"
-            alt="frame_logo"
-          />
-          <h1 class="frame__content-item_main-title">
-            <p class="frame__content-item_main-title-first-element">
+    <div className="frame container">
+      <div className="frame__content">
+        <div className="frame__content-item">
+        <SquaresLogotype />
+          <h1 className="frame__content-item_main-title">
+            <p className="frame__content-item_main-title-first-element">
               Магическая{" "}
             </p>
-            <p class="frame__content-item_main-title-second-element">
+            <p className="frame__content-item_main-title-second-element">
               Атмосфера
             </p>
           </h1>
-          <h2 class="frame__content-description">
+          <h2 className="frame__content-description">
             В нашем заведении царит магическая атмосфера наполненная вкусными
             ароматами
           </h2>
         </div>
-        <div class="frame__content-item">
-          <img
-            class="frame__content-item_pic"
-            src="frame_pic.png"
-            alt="frame_logo"
-          />
-          <h1 class="frame__content-item_main-title">
-            <p class="frame__content-item_main-title-first-element">
+        <div className="frame__content-item">
+        <SquaresLogotype />
+          <h1 className="frame__content-item_main-title">
+            <p className="frame__content-item_main-title-first-element">
               Лучшее качество
             </p>
-            <p class="frame__content-item_main-title-second-element">Еды</p>
+            <p className="frame__content-item_main-title-second-element">Еды</p>
           </h1>
-          <h2 class="frame__content-description">
+          <h2 className="frame__content-description">
             Качество нашей Еды - отменное!
           </h2>
         </div>
-        <div class="frame__content-item">
-          <img
-            class="frame__content-item_pic"
-            src="frame_pic.png"
-            alt="frame_logo"
-          />
-          <h1 class="frame__content-item_main-title">
-            <p class="frame__content-item_main-title-first-element">
+        <div className="frame__content-item">
+        <SquaresLogotype />
+          <h1 className="frame__content-item_main-title">
+            <p className="frame__content-item_main-title-first-element">
               Недорогая{" "}
             </p>
-            <p class="frame__content-item_main-title-second-element">Еда</p>
+            <p className="frame__content-item_main-title-second-element">Еда</p>
           </h1>
-          <h2 class="frame__content-description">
+          <h2 className="frame__content-description">
             Стоимость нашей Еды зависит только от ее количества. Качество всегда
             на высоте!
           </h2>
