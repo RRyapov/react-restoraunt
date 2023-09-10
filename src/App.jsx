c5eb6383456566dc11e52114a9ca5f6bf760b510
RRyapov <rryapov@gmail.com>
2023-09-10 21:45:45 +0300

    Создание раздела Наша история

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css";
 import { Header } from "./components/header/Header";
 import { Squares } from "./components/body/square_tables/Squares";
+import { OurHistory } from "./components/body/our_history/History"
 
 export const App = () => {
   return (
     <>
       <Header />
       <Squares />
+      <OurHistory />
     </>
   );
 };
---
+++ src/components/body/our_history/History.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./history_style.css";
+import { HistPictures } from "./HistoryPics";
+
+export const OurHistory = () => {
+  return (
+    <div class="block_3">
+      <div class="block-text">
+        <div class="description-content">
+          <h1 class="description__main-title">
+            <p class="description__first-element">Наша</p>
+            <p class="description__second-element">История</p>
+          </h1>
+          <h2 class="description_second-title">
+            Как и у любого другого самобытного места, у нас есть своя, особая
+            история. Идея ресторана пришла основателям неожиданно. Во время
+            прогулки по лесу создатель нашего ресторана застрял в сотнях
+            километров от ближайшего населенного пункта. Вдали от цивилизации и
+            связи им пришлось навремя обустровать себе нехитрый быт, добывать и
+            готовить себе еду.
+          </h2>
+        </div>
+        <div class="description-info">
+          <div class="description-info__first-title">
+            93
+            <span class="description-info__second-title">Напитки</span>
+          </div>
+          <div class="description-info__first-title">
+            206
+            <span class="description-info__second-title">Еда</span>
+          </div>
+          <div class="description-info__first-title">
+            71
+            <span class="description-info__second-title">Закуски</span>
+          </div>
+        </div>
+      </div>
+      <div class="block-img">
+        <HistPictures />
+      </div>
+    </div>
+  );
+};
