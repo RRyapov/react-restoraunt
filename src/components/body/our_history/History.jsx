00c9740cda82bb43415181daa79801a66d9907ef
RRyapov <rryapov@gmail.com>
2023-09-13 23:04:06 +0300

    Добавлен useState к картинкам

--- src/components/body/our_history/History.jsx
+++ src/components/body/our_history/History.jsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./history_style.css";
 import { HistPictures } from "./HistoryPics";
 
 export const OurHistory = () => {
+  const [isShowingText, setShowingText] = useState(false);
   return (
     <div class="block_3">
       <div class="block-text">
         <div class="description-content">
           <h1 class="description__main-title">
             <p class="description__first-element">Наша</p>
-            <p class="description__second-element">История</p>
+            <p class="description__second-element"
+            onClick={() => 
+            setShowingText((prevIsShowingText) => !prevIsShowingText)
+            }
+            >История</p>
           </h1>
           <h2 class="description_second-title">
-            Как и у любого другого самобытного места, у нас есть своя, особая
-            история. Идея ресторана пришла основателям неожиданно. Во время
-            прогулки по лесу создатель нашего ресторана застрял в сотнях
-            километров от ближайшего населенного пункта. Вдали от цивилизации и
-            связи им пришлось навремя обустровать себе нехитрый быт, добывать и
-            готовить себе еду.
+            {" "}
+            {isShowingText && (
+              <>
+                Как и у любого другого самобытного места, у нас есть своя,
+                особая история. Идея ресторана пришла основателям неожиданно. Во
+                время прогулки по лесу создатель нашего ресторана застрял в
+                сотнях километров от ближайшего населенного пункта. Вдали от
+                цивилизации и связи им пришлось навремя обустровать себе
+                нехитрый быт, добывать и готовить себе еду.
+              </>
+            )}
           </h2>
         </div>
         <div class="description-info">
